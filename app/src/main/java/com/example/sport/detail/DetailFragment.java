fae9396ec1a32f85abb5df55003cef33bb997d87
rajesh <rajesh.k.khadka@gmail.com>
2020-06-08 10:04:47 +0545

    refactor method

--- app/src/main/java/com/example/sport/detail/DetailFragment.java
+++ app/src/main/java/com/example/sport/detail/DetailFragment.java
@@ -12,6 +12,7 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.sport.data.remote.Team;
 import com.example.sport.databinding.FragmentDetailBinding;
+import com.example.sport.utils.AppUtils;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -39,33 +40,12 @@ public class DetailFragment extends Fragment {
     }
 
     void setupRecyclerView() {
-        ArrayList<String> leagues = getLeagues();
-        LeagueAdapter leagueAdapter = new LeagueAdapter(leagues);
+        LeagueAdapter leagueAdapter = new LeagueAdapter(AppUtils.getLeagues(mTeam));
         mBinding.recyclerLeague.setAdapter(leagueAdapter);
         mBinding.btnUpcomingEvents.setOnClickListener(v -> {
             NavHostFragment.findNavController(this).navigate(DetailFragmentDirections.actionDetailFragmentToEventFragment(mTeam.getIdTeam()));
         });
     }
 
-    @NotNull
-    private ArrayList<String> getLeagues() {
-        Class aClass = Team.class;
-        ArrayList<String> leagues = new ArrayList<>();
-        try {
-            //apis only shows the 7 league played by team
-            for (int i = 1; i <= 7; i++) {
-                Field field = aClass.getDeclaredField(i == 1 ? "strLeague" : "strLeague" + i);
-                field.setAccessible(true);
-                Object value = field.get(mTeam);
-                if (value != null) {
-                    leagues.add(value.toString());
-                }
-            }
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        return leagues;
-    }
+
 }
